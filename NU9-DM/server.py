910d26e277b3e51bc965df43ea93c70e21a0cca8
BeanWei <157318439@qq.com>
2018-05-05 15:21:58 +0800

    :rocket:测试曲线可视化完成

--- NU9-DM/server.py
+++ NU9-DM/server.py
@@ -7,13 +7,18 @@ toDay = time.strftime("%Y%m%d")
 
 from flask import Flask, render_template, session, request
 from flask_socketio import SocketIO, emit
-from flask_pymongo import PyMongo
+# from flask_pymongo import PyMongo
+import pymongo
 
 async_mode = None
 
 app = Flask(__name__) 
 #使用默认的mongodb的配置 
-mongo = PyMongo(app)
+# mongo = PyMongo(app)
+
+with app.app_context():
+    conn= pymongo.MongoClient('localhost', 27017)
+    db = conn.test
 
 socketio = SocketIO(app, async_mode=async_mode)
 
@@ -23,27 +28,56 @@ thread_lock = Lock()
 
 def call_data():
     '''从数据库中获取数据推送至前台'''
+    count = 0
     series = []
     while True:
         #缓冲时间
-        socketio.sleep(300)
+        socketio.sleep(10)
+        count += 1
         #获取y轴的数据
-        for result in mongo.db.result.find({"date": toDay}):
+        for result in db.result.find({"date": toDay}):
             if time.strftime("%H%m") == "0000":
                 series = []
-            sn = result.sn
-            logtime = result.logtime
-            data = mongo.db.data.find_one({"sn": sn, "logtime": logtime})            #TODO:logtime可能存在不等的情况
-            yaxis = data.yaxis
-            series.append({"name": sn,"type": "line", "data": yaxis})
+            sn = result["sn"]
+            logtime = result["logtime"]
+            #data = db.data.find_one({"sn": sn, "logtime": logtime})            #TODO:logtime可能存在不等的情况
+            data = db.data.find_one({"sn": sn})
+            if data: 
+                yaxis = data["yaxis"]
+                series.append({
+                    "name": sn,
+                    "symbol":'none', 
+                    "type": "line", 
+                    "smooth": True, 
+                    "lineStyle": {"color": "#00CD00"}, 
+                    "data": yaxis
+                    #TODO:增加辅助线（LIMMIT）
+                    # "markLine": {
+                    #     "name": "limmit",
+                    #     "lineStyle": {"color": "red"}, 
+                    #     "xAxis": {
+                    #         "data": [100.000,106.000,112.000,118.000,125.000,132.000,140.000,150.000,160.000,170.000,180.000,190.000,200.000,212.000,224.000,236.000,250.000,265.000,280.000,300.000,315.000,335.000,355.000,375.000,400.000,425.000,450.000,475.000,500.000,530.000,560.000,600.000,630.000,670.000,710.000,750.000,800.000,850.000,900.000,950.000,1000.000,1060.000,1120.000,1180.000,1250.000,1320.000,1400.000,1500.000,1600.000,1700.000,1800.000,1900.000,2000.000]
+                    #     },
+                    #     "yAxis": {
+                    #         "data": [0.224,0.199,0.303,0.353,0.299,0.652,0.586,0.618,0.632,0.469,0.370,0.403,0.367,0.312,0.249,0.236,0.252,0.188,0.172,0.118,0.143,0.093,0.077,0.070,0.044,0.056,0.040,0.062,0.041,0.052,0.041,0.025,0.046,0.035,0.030,0.029,0.030,0.032,0.023,0.026,0.018,0.028,0.028,0.024,0.026,0.029,0.040,0.010,0.021,0.022,0.009,0.011,0.006]
+                    #     }
+                    #     # "data": [
+                    #     #     {
+                    #     #         "xAxis": [100.000,106.000,112.000,118.000,125.000,132.000,140.000,150.000,160.000,170.000,180.000,190.000,200.000,212.000,224.000,236.000,250.000,265.000,280.000,300.000,315.000,335.000,355.000,375.000,400.000,425.000,450.000,475.000,500.000,530.000,560.000,600.000,630.000,670.000,710.000,750.000,800.000,850.000,900.000,950.000,1000.000,1060.000,1120.000,1180.000,1250.000,1320.000,1400.000,1500.000,1600.000,1700.000,1800.000,1900.000,2000.000],
+                    #     #         "yAxis": [0.224,0.199,0.303,0.353,0.299,0.652,0.586,0.618,0.632,0.469,0.370,0.403,0.367,0.312,0.249,0.236,0.252,0.188,0.172,0.118,0.143,0.093,0.077,0.070,0.044,0.056,0.040,0.062,0.041,0.052,0.041,0.025,0.046,0.035,0.030,0.029,0.030,0.032,0.023,0.026,0.018,0.028,0.028,0.024,0.026,0.029,0.040,0.010,0.021,0.022,0.009,0.011,0.006]
+                    #     #     }
+                    #     # ]
+                    # }
+                })
+            else:
+                continue
 
         #获取当天的产量和Spk RB 的fail数量 TODO:提供日期的范围查询
-        total = mongo.db.result.find({"date": toDay}).count()
-        fail = mongo.db.result.find({"date": toDay, "result": "FAIL"}).count()
-
+        total = db.result.find({"date": toDay}).count()
+        fail = db.result.find({"date": toDay, "result": "FAIL"}).count()
         socketio.emit(
-            "server_response",
-            {"todayTotal": total, "todayFail": fail, "logs": series},
+            "my_response",
+            {"data":{"todayTotal": total, "todayFail": fail, "yaxis": series}, "count": count},
             namespace='/show'
         )
 
@@ -59,6 +93,7 @@ def toconnect():
     with thread_lock:
         if thread is None:
             thread =socketio.start_background_task(target=call_data)
+    emit("my_response", {"data": 'Connected', "count": 0})
 
 
 if __name__ == "__main__":
--- NU9-DM/store_data.py
+++ NU9-DM/store_data.py
@@ -4,7 +4,7 @@ import os
 import shutil
 import time
 
-server_path = ""
+server_path = ".\\logs"
 
 #获取当前时间来分类文件
 toDay = time.strftime("%Y%m%d")
@@ -12,48 +12,59 @@ toDay = time.strftime("%Y%m%d")
 def get_data():
     '''操作server上的log并获取数据
     '''
-    for (root, dirs, files) in os.walk(server_path):
-        if toDay not in dirs:
-            os.mkdir(toDay)
-        for f in files:
-            sn = f.split('_')[3]
-            logtime = f.split('_')[-1]
-            path = os.path.join(server_path,f)
-            with open(path) as fp:
+    while True:
+        for (root, dirs, files) in os.walk(server_path, topdown=False):
+            
+            if toDay not in dirs:
                 try:
-                    content = fp.readlines()
-                    if "Result" in f:
-                        handle_result(content, sn, logtime)
-                    elif "Data" in f:
-                        handle_data(content, sn, logtime)
-                except:
-                    shutil.move(path, os.path.join(server_path,toDay))
-            #移动文件至今日建立的文件夹
-            shutil.move(path, os.path.join(server_path,toDay))
+                    os.mkdir(server_path+"\\"+toDay)
+                except OSError:
+                        pass
+        if files:
+            for f in files:
+                sn = f.split('_')[2]
+                logtime = (f.split('_')[-1]).split('.')[0]
+                path = os.path.join(server_path,f)
+                with open(path) as fp:
+                    try:
+                        content = []
+                        for l in fp.readlines():
+                            content.append(l.split(','))
+                        if "Result" in f:
+                            handle_result(content, sn, logtime)
+                        elif "Data" in f:
+                            handle_data(content, sn, logtime)
+                    except:
+                        shutil.move(path, os.path.join(server_path,toDay))
+                        print("异常文件%s已被移动" % f)
+                        continue
+                #移动文件至今日建立的文件夹
+                shutil.move(path, os.path.join(server_path,toDay))
+                print("%s处理成功并移至当日文件夹" % f)
+        else:
+            pass    
 
 
 
 def handle_result(content, sn, logtime):
     '''处理result'''
-    c_content = list(zip(*content))   #行列转置
-    #TODO: log中的具体行列
-    if c_content[0].count("FAIL") == 1 and c_content[0][0] == "FAIL":
+    content = list(zip(*content))
+    if content[3].count("FAIL") == 1 and content[3][3] == "FAIL":
         result = "FAIL"
     else:
         result = "PASS"
-
     if db.result.find_one({"sn": sn}):
         return 
     db.result.save({"date": toDay, "sn": sn, "logtime": logtime, "result": result})
-
+    
 
 
 def handle_data(content, sn, logtime):
     '''处理data'''
-    if "Spk Rub&Buzz" in content[0]:
-        yaxis = content[0 + 2]
-
+    if "Spk Rub" in content[9][0]:
+        yaxis = content[11][0:53]
         db.data.save({"sn": sn, "logtime": logtime, "yaxis": yaxis})
+        
 
 conn = pymongo.MongoClient("127.0.0.1", 27017)
 db = conn.test
