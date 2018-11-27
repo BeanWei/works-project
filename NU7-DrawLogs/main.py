470be3804498a15fd881d346a78b2d6c500bc516
BeanWei <157318439@qq.com>
2018-11-27 21:05:11 +0800

    :memo: 20181127

---
+++ NU7-DrawLogs/main.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+
+"""
+# dev: python2
+# @Bean.Wei
+"""
+
+from pyecharts import Line
+
+#values
+attr = ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+v1 = [5, 20, 36, 10, 10, 100]
+v2 = [55, 60, 16, 20, 15, 80]
+
+line = Line("折线图示例")
+line.add("商家A", attr, v1, mark_point=["average"])
+line.add("商家B", attr, v2, is_smooth=True, mark_line=["max", "average"])
+line.render("./html/result.html")
