3964d1c6efe3b51a96cee13aac3f5c056d8b1f86
BeanWei <157318439@qq.com>
2018-11-29 01:26:36 +0800

    update

--- NU7-DrawLogs/main.py
+++ NU7-DrawLogs/main.py
@@ -8,16 +8,22 @@
 """
 
 import os
+import shutil
+import re
+import threading
 
 import logging
 logging.basicConfig(level = logging.INFO, format = "[%(asctime)s]%(levelname)s : %(message)s")
 logger = logging.getLogger(__name__)
 
+# define the json file template
+jsonTemp = '{"name": "%s","type": "line","symbol": "circle","symbolSize": 4,"itemStyle": {"normal": {"color": "%s"}},"data": [%s]}'
+
 # logs dir: save the logs
-# make json dir: ending create the json data file
-# target html: open the main html in browser
 Logs_Dir = "./Logs"
+# make json dir: ending create the json data file
 MakeJson_Dir = "./jsonData"
+# target html: open the main html in browser
 Target_Html = "NU7.html"
 
 # logs item is the logs txt name.
@@ -53,9 +59,9 @@ def main():
     if not logs:
         logger.info("NOT FOUND THE LOGS, WILL EXIST")
         os._exit(0)
-    for log in logs:
-        #with open  
-        pass  
+    logSet = setLogs(logs)  
+    for k, v in logSet.items():
+        makeJsonFiles(k, v)
 
 def init():
     """check all the Dirs
@@ -68,17 +74,79 @@ def init():
         logger.info("check the Logs Dir whether exist: NO ")  
         logger.info("No logs Dir! Will exit")  
         os._exit(0)    
-    if not os.path.exists(MakeJson_Dir):
-        os.makedirs(MakeJson_Dir)
+    if os.path.exists(MakeJson_Dir):
+        shutil.rmtree(MakeJson_Dir)
+    os.makedirs(MakeJson_Dir)
     if not os.path.exists(Target_Html):    
         # TODO: Make a html if no exist
         pass
     logger.info("Init OK!")    
 
-def checkLogs(logsnamelist):
+def setLogs(logsnamelist):
+    """
+    get the logs file set. 
+    eg: {"Left_Mic_THD": ["Left_Mic_THD_2018_11_07.txt", Left_Mic_THD_2018_11_05.txt]}
+
+    args:
+        the logs name list
+    return:
+        the dict set    
+    """
     logs = ",".join(logsnamelist)
+    logsDic = dict()
     for item in LOGS_ITEM:
-        pass
+        val = re.findall(r'%s.*?.txt' % item, logs)
+        if val:
+            logsDic[item] = val 
+    if not logsDic:
+        logger.info("Not Found the test Logs in the Logs folder, Will exit!")
+        os._exit(0)
+    return logsDic    
+
+def readSingleLog(file_path):
+    """raed the log, get the test value
+    args:
+        file_path: the log path
+    return:
+        a dict, {sn_title: test_value}    
+    """    
+    # TODO: Protece the code: out off the range, check the test values length.
+    with open(file_path) as fp:
+        f = fp.readlines()
+        # remove the fist line=> yaxis infos, and the second line=> xaxis infos
+        f = f[2:]
+        logInfoDic = dict()
+        for line in f:
+            cut = line.strip(",").split(",,")
+            if len(cut) == 2:
+                sn_title, values = cut
+                logInfoDic[sn_title] = values    
+            else:
+                print("this line can't match", line)   
+        return logInfoDic    
+
+
+def makeJsonFiles(output_filename, input_logsname_list):
+    """
+    read the logs, create the json files.
+    args:
+        output_filename: the json file name
+        input_logsname: the list of same item logs
+    return:    
+    """
+    jsonContainStr = "["
+    for logname in input_logsname_list:
+        logInfoDic = readSingleLog(os.path.join(Logs_Dir, logname))
+        for sninfo, testvalue in logInfoDic.items():
+            # TODO: make the random color to the line
+            item = jsonTemp % (sninfo, "green", testvalue)
+            jsonContainStr += item + ","    
+    jsonContainStr = jsonContainStr.rstrip(",") + "]"
+    # TODO: Need to check this way is OK?
+    opt_fn = os.path.join(MakeJson_Dir, output_filename)
+    with open(opt_fn , 'w') as fp:
+        fp.write(jsonContainStr)
+    os.rename(opt_fn, opt_fn + ".json")
 
 
 if __name__ == '__main__':
--- NU7-DrawLogs/static/js/showSingle.js
+++ NU7-DrawLogs/static/js/showSingle.js
@@ -7,8 +7,8 @@ function showSingleBtn() {
         myChart.setOption(option = {
             //标题
             title: {
-                text: 'Bean.Wei',
-                subtext: 'demo test'
+                text: 'NU7-Audio',
+                subtext: 'QCMC·AudioTeam'
             },
             tooltip: {
                 trigger: 'axis'
@@ -22,8 +22,9 @@ function showSingleBtn() {
                 y: 'top',
                 selectedMode: 'single',
                 //selected: selected,
-                width: 300,
-                align: 'auto',
+                //width: 50,
+                //align: 'auto',
+                align: 'left',
                 borderColor: 'rgba(178,34,34,0.8)',
                 borderWidth: 4,
                 padding: 10,    // [5, 10, 15, 20]
@@ -34,9 +35,9 @@ function showSingleBtn() {
                 })
             },
             grid: {
-                //top: '10%',
-                left: '3%',   //图表距边框的距离
-                right: '150',
+                top: '10%',
+                left: '1%',   //图表距边框的距离
+                right: '26%',
                 bottom: '10%',
                 containLabel: true
             },
@@ -55,7 +56,7 @@ function showSingleBtn() {
             xAxis: {
                 type: 'category',
                 boundaryGap: false,
-                data: ['12-01','12-02','12-03','12-04','12-05','12-05','12-06','12-07','12-08','12-09','12-10','12-11','12-12','12-13'],
+                data: [100.00,106.00,112.00,118.00,125.00,132.00,140.00,150.00,160.00,170.00,180.00,190.00,200.00,212.00,224.00,236.00,250.00,265.00,280.00,300.00,315.00,335.00,355.00,375.00,400.00,425.00,450.00,475.00,500.00,530.00,560.00,600.00,630.00,670.00,710.00,750.00,800.00,850.00,900.00,950.00,1000.00,1060.00,1120.00,1180.00,1250.00,1320.00,1400.00,1500.00,1600.00,1700.00,1800.00,1900.00,2000.00,2120.00,2240.00,2360.00,2500.00,2650.00,2800.00,3000.00,3150.00,3350.00,3550.00,3750.00,4000.00,4250.00,4500.00],
                 //坐标轴颜色
                 axisLine:{
                     lineStyle:{
@@ -73,7 +74,7 @@ function showSingleBtn() {
                 {
                     type : 'value',
                     axisLabel : {
-                        formatter: '{value} 人'
+                        formatter: '{value} db'
                     }
                 }
             ],
